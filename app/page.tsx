1c5514cc5ee23a82bd40cd542da0b6026d9c95b8
Roni Ardiyanto <35429278+infrasync@users.noreply.github.com>
2023-08-15 16:40:50 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,71 @@
+"use client"
+
+import Link from "next/link"
+import { Select, TextInput, createStyles } from "@mantine/core"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: "relative",
+  },
+
+  input: {
+    height: "auto",
+    paddingTop: 18,
+  },
+
+  label: {
+    position: "absolute",
+    pointerEvents: "none",
+    fontSize: theme.fontSizes.xs,
+    paddingLeft: theme.spacing.sm,
+    paddingTop: theme.spacing.sm,
+    zIndex: 1,
+  },
+}))
+
+export default function IndexPage() {
+  const { classes } = useStyles()
+
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Beautifully designed components <br className="hidden sm:inline" />
+          built with Radix UI and Tailwind CSS.
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Accessible and customizable components that you can copy and paste
+          into your apps. Free. Open Source. And Next.js 13 Ready.
+        </p>
+        <Select
+          style={{ marginTop: 20, zIndex: 2 }}
+          data={["React", "Angular", "Svelte", "Vue"]}
+          placeholder="Pick one"
+          label="Your favorite library/framework"
+          classNames={classes}
+        />
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={siteConfig.links.docs}
+          target="_blank"
+          rel="noreferrer"
+          className={buttonVariants()}
+        >
+          Documentation
+        </Link>
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: "outline" })}
+        >
+          GitHub
+        </Link>
+      </div>
+    </section>
+  )
+}
